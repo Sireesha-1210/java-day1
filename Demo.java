58359dd614d255308ffb6a38a7eee7aea9d011f2
Sireesha-1210 <shireeshasiri761@gmail.com>
2026-01-31 14:29:21 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,7 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("java programming");
+        System.out.print("hello");
+        System.out.println("world");
+    }
+}
