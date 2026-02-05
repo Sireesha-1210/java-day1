a0710b98ca02ad351824f890fedc121403b1778b
Sireesha-1210 <shireeshasiri761@gmail.com>
2026-02-05 11:50:54 +0530

    Add files via upload

---
+++ MinArray.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MinArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter comma seperated integers:");
+        String input=sc.nextLine();
+        String[] strArr=input.split(",");
+        int[] arr=new int[strArr.length];
+        for(int i=0;i<strArr.length;i++)
+        {
+            arr[i]=Integer.parseInt(strArr[i].trim());
+        }
+        System.out.println("Minimum element:"+findMin(arr));
+    }
+    public static int findMin(int[] arr){
+        int min=arr[0];
+        for(int num:arr){
+            if(num<min)min=num;
+        }
+        return min;
+    }
+}
