1f0cba6cf6d3f776a4d5b8fdf0d52c0e2eed9664
Sireesha-1210 <shireeshasiri761@gmail.com>
2026-02-07 11:03:03 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,24 @@
+public class Primitive {
+    public static void main(String[] args) {
+        byte b=120;
+        short s=324;
+        int i=3435546;
+        long l=32436554;
+        float f=83.34f;
+        double d=43.3423;
+        char c='*';
+        char character='s';
+        boolean bl=true;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(character);
+        System.out.println(bl);
+
+    }
+    
+}
