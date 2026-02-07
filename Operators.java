04bba90cc7210f726ff2509a992856f37f322968
Sireesha-1210 <shireeshasiri761@gmail.com>
2026-02-07 15:28:40 +0530

    Add files via upload

--- Operators.java
+++ Operators.java
@@ -1,15 +1,34 @@
+import java.util.*;
 public class Operators {
     public static void main(String[] args) {
-        int a=52;
-        int b=43;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter first number:");
+        int a=sc.nextInt();
+        System.out.println("enter second number:");
+        int b=sc.nextInt();
         System.out.println(a+b);
-        if(a>0){
-            System.out.println("given number is positive.");
+        System.out.println("enter a number:");
+        int num=sc.nextInt();
+        if(num>0){
+            System.out.println("Number is positive.");
         }
-        if(a>0&&a%2==0){
-            System.out.println("given number is positive and even.");
+
+
+        System.out.println("enter a number1:");
+        int num1=sc.nextInt();
+        if(num1>0&&num1%2==0){
+            System.out.println("Number is positive and even.");
         }
-        System.out.println(b=54);
-        System.out.println(++a);
+
+
+        System.out.println("enter a number:");
+        int x=sc.nextInt();
+        System.out.println("Assigned value="+x);
+
+        
+        System.out.println("enter a number:");
+        int y=sc.nextInt();
+        y++;
+        System.out.println("After increment:"+y);
     }
 }
