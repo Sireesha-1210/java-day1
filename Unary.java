cbd294ede8bd31e34b133435c06258d2c1c60cb2
Sireesha-1210 <shireeshasiri761@gmail.com>
2026-02-07 14:35:43 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,12 @@
+public class Unary {
+    public static void main(String[] args) {
+        long a=11;
+        System.out.println(+a);
+        System.out.println(-a);
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(--a);
+        System.out.println(a--);
+
+    }
+}
