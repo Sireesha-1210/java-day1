eac55311a3842d55ada3b9685bce1488f7bcab50
Sireesha-1210 <shireeshasiri761@gmail.com>
2026-02-07 14:35:05 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,15 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a=52;
+        int b=43;
+        System.out.println(a+b);
+        if(a>0){
+            System.out.println("given number is positive.");
+        }
+        if(a>0&&a%2==0){
+            System.out.println("given number is positive and even.");
+        }
+        System.out.println(b=54);
+        System.out.println(++a);
+    }
+}
