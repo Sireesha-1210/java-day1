32d0b916d01d666b2d3a502106ac4e08494b73d3
Sireesha-1210 <shireeshasiri761@gmail.com>
2026-02-07 11:03:40 +0530

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,14 @@
+public class Example1 {
+    public static void main(String[] args) {
+        String name="sireesha";
+        short age=19;
+        char grade='A';
+        int marks=90;
+        String status="pass";
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(grade);
+        System.out.println(marks);
+        System.out.println(status);
+    }  
+}
