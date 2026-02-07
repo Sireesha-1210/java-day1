4e90bf5d60c89246c5c533e3ee83c6a20222e3f2
Sireesha-1210 <shireeshasiri761@gmail.com>
2026-02-07 11:03:24 +0530

    Add files via upload

---
+++ Example2.java
@@ -0,0 +1,11 @@
+public class Example2 {
+    public static void main(String[] args) {
+        int s1=76;
+        int s2=98;
+        int s3=34;
+        int total=s1+s2+s3;
+        double average=total/3;
+        System.out.println(total);
+        System.out.println(average);
+    }
+}
