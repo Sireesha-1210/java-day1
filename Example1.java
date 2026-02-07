483001ecea1f287293205b3d79cd6f64cbe2c257
Sireesha-1210 <shireeshasiri761@gmail.com>
2026-02-07 13:41:35 +0530

    Add files via upload

--- Example1.java
+++ Example1.java
@@ -1,14 +1,23 @@
+import java.util.Scanner;
 public class Example1 {
     public static void main(String[] args) {
-        String name="sireesha";
-        short age=19;
-        char grade='A';
-        int marks=90;
-        String status="pass";
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter name:");
+        String name=sc.nextLine();
+        System.out.println("enter age:");
+        Short age=sc.nextShort();
+        System.out.println("enter marks:");
+        int marks=sc.nextInt();
+        System.out.println("enter grade:");
+        char grade=sc.next().charAt(0);
+        System.out.println("enter status:");
+        String status=sc.next();
+        sc.close();
         System.out.println(name);
         System.out.println(age);
-        System.out.println(grade);
         System.out.println(marks);
+        System.out.println(grade);
         System.out.println(status);
-    }  
+    }
+
 }
