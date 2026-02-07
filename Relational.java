b4041183ef9649bb7fb76485865484d49ddd9c3d
Sireesha-1210 <shireeshasiri761@gmail.com>
2026-02-07 14:35:25 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,12 @@
+public class Relational {
+    public static void main(String[] args) {
+        float a=4.4f;
+        float b=45.65f;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a>=b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
+    }
+}
