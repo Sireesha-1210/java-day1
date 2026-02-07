9abdea7e140c6984f44b5043fbc269d47d16ba23
Sireesha-1210 <shireeshasiri761@gmail.com>
2026-02-07 13:41:54 +0530

    Add files via upload

--- Example2.java
+++ Example2.java
@@ -1,9 +1,14 @@
-public class Example2 {
-    public static void main(String[] args) {
-        int s1=76;
-        int s2=98;
-        int s3=34;
-        int total=s1+s2+s3;
+import java.util.Scanner;
+public class Example2 {   
+     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter subject1 marks:");
+        int subject1=sc.nextInt();
+        System.out.println("enter subject2 marks:");
+        int subject2=sc.nextInt();
+        System.out.println("enter subject3 marks:");
+        int subject3=sc.nextInt();
+        int total=subject1+subject2+subject3;
         double average=total/3;
         System.out.println(total);
         System.out.println(average);
