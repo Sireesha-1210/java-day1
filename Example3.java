f2bb62e379b37009baa00a157b069105f6a9e79e
Sireesha-1210 <shireeshasiri761@gmail.com>
2026-02-07 11:11:38 +0530

    Add files via upload

---
+++ Example3.java
@@ -0,0 +1,12 @@
+public class Example3 {
+    public static void main(String[] args) {
+        String moviename="kerintha";
+        int seatnumber=4;
+        int ticketprice=4534;
+        boolean b=true;
+        System.out.println(moviename);
+        System.out.println(seatnumber);
+        System.out.println(ticketprice);
+        System.out.println(b);
+    }
+}
