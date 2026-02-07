3484be8d4047ec5cd792f2608adc3e7a7b1b4cff
Sireesha-1210 <shireeshasiri761@gmail.com>
2026-02-07 14:34:00 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,9 @@
+public class Assignment {
+    public static void main(String[] args) {
+        int a=45;
+        System.out.println(a+=2);
+        System.out.println(a-=43);
+        System.out.println(a/=4);
+        System.out.println(a*=6);
+    }
+}
