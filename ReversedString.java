dbd76fa5aebb4979b30031e3a4330da44e735bdb
Sireesha-1210 <shireeshasiri761@gmail.com>
2026-02-28 15:57:21 +0530

    Add files via upload

---
+++ ReversedString.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ReversedString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a String:");
+        String str=sc.nextLine();
+        String reversed="";
+        for(int i=str.length()-1;i>=0;i--){
+            reversed=reversed+str.charAt(i);
+        }
+        System.out.println("Reversed String:"+reversed);
+        sc.close();
+    }
+}
+
